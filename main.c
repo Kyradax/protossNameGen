053413c5416f660ed9d65890b1734c006cd88033
Kyradax <pololessuper@gmail.com>
2016-02-09 13:40:49 +0100

    second commit

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main (int argc, char *argv[])
 	int a;
 
 	char *first[11] = {"Ze","Ar","Ta","Se","Al","Vo","Al","Ul","Ras", "Ta","Ky"};
-	char *second[11] = {"ra","ta","ssa","len","da","a","re","za","fe","a","mo"};
-	char *third[14] = {"dax","tul","nis","dar","dis","ris","zun","rak","zaj","gal","nix","dun","khas","jo"};
+	char *second[12] = {"ra","ta","ssa","len","da","a","re","za","fe","a","mo","ka"};
+	char *third[15] = {"dax","tul","nis","dar","dis","ris","zun","rak","zaj","gal","nix","dun","khas","jo","rax"};
 
 	char *name[3];
 	srand(time(NULL));
